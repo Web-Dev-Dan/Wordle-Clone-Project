8a38fcf2fd48af86f65363fffff6930970c9d5db
Dan <80458000+Web-Dev-Dan@users.noreply.github.com>
2022-02-09 22:41:48 +0900

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,36 @@
+const words = [
+    'Hello',
+    'Japan',
+    'China'
+]
 
+// ---------- SUBMITTING A GUESS ----------
+document.addEventListener('keydown', keyPressed);
+document.addEventListener('click', mouseClicked);
+
+// Press Key ⬜️
+function keyPressed(e) {
+    if (e.key.match(/[a-z]/)) {
+        console.log(`Letter ${e.key} pressed`)
+    }
+    if (e.key === 'Enter') {
+        console.log('Enter pressed');
+    }
+    if (e.key === 'Backspace') {
+        console.log('Delete pressed');
+    }
+}
+
+// Click Mouse 🖱
+function mouseClicked(e) {
+    if (e.target.matches('[data-key]')) {
+        console.log(`Letter ${e.target.textContent} pressed`);
+    }
+    if (e.target.matches('[data-enter]')) {
+        console.log('Enter pressed');
+    }
+    if (e.target.matches('[data-delete')) {
+        // Check bug when clicking the SVG icon
+        console.log('Delete pressed');
+    }
+}
